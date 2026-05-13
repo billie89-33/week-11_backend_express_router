004d52f4ab386ecf10c9ac6e20dc021752d1dac0
billie89-33 <bowvorn@gmail.com>
2026-05-13 15:05:56 +0700

    fix gitignore

---
+++ src/fakeData/fakeUser.js
@@ -0,0 +1,22 @@
+const users = [
+{
+    id: "1",
+    username: "bank",
+    email: "bank@gmail.com",
+    password:"pass123"
+},
+{
+    id: "1",
+    username: "bank1",
+    email: "bank1@gmail.com",
+    password:"pass123"
+},
+{
+    id: "1",
+    username: "bank2",
+    email: "bank2@gmail.com",
+    password:"pass123"
+}
+];
+
+export default users;
